f357cf44679b5a9c35e4e496b49ae810b5525fa9
seghost <deimos12399@gmail.com>
2023-08-26 13:08:54 -0300

    code reform

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import requests
 
-apiKey = "7beb253c580f44bcb5d3eacf1acc9e74"
-cityName = "Chicago"
+apiKey = "Insert your OpenWeather API key here"
+cityName = "Canada"
 amountOfResults = 1
-geoData = None
-weatherData = None
 
-geoUrl = f"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit={amountOfResults}&appid={apiKey}"
 
-response = requests.get(geoUrl)
+def get_geo_data(cityname, amount_of_results, api_key):
+    geoUrl = f"http://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit={amount_of_results}&appid={api_key}"
+    response = requests.get(geoUrl)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Geo Data request failed with status code: ", response.status_code)
 
-if response.status_code == 200:
-    geoData = response.json()
-    print(geoData)
-else:
-    print("Request failed with status code: ", response.status_code)
 
-Url = f"https://api.openweathermap.org/data/2.5/weather?lat={geoData[0]['lat']}&lon={geoData[0]['lon']}&appid={apiKey}&units=metric"
+def get_weather_data(lat, lon, api_key):
+    weatherUrl = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+    response = requests.get(weatherUrl)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Weather Data request failed with status code: ", response.status_code)
 
-response = requests.get(Url)
 
-if response.status_code == 200:
-    weatherData = response.json()
-    print(f"Weather in {cityName} is " + weatherData['weather'][0]['description'] + f" and the temperature is {weatherData['main']['temp']}º celsius")
-else:
-    print("Request failed with status code: ", response.status_code)
+geoData = get_geo_data(cityName, amountOfResults, apiKey)
+weatherData = get_weather_data(geoData[0]['lat'], geoData[0]['lon'], apiKey)
+
+print(geoData)
+print(f"Weather in {cityName} is " + weatherData['weather'][0]['description'] + f" and the temperature is {weatherData['main']['temp']}º celsius whit a humidity of {weatherData['main']['humidity']}%")
