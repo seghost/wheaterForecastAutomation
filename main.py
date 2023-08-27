3b52d0cd04708aa83df516c478f3def3e31ec8c8
seghost <deimos12399@gmail.com>
2023-08-26 23:51:09 -0300

    code refactor and accuweather comparation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
 
-apiKey = "Place your openWeather API key here"
-cityName = "Chicago"
+apiKey = "Insert your OpenWeather API key here"
+cityName = "Buenos Aires"
 amountOfResults = 1
+
+driver = webdriver.Chrome()
 
 
 def get_geo_data(cityname, amount_of_results, api_key):
@@ -25,6 +32,29 @@ def get_weather_data(lat, lon, api_key):
 
 geoData = get_geo_data(cityName, amountOfResults, apiKey)
 weatherData = get_weather_data(geoData[0]['lat'], geoData[0]['lon'], apiKey)
+driver.get("https://www.accuweather.com/")
+
+wait = WebDriverWait(driver, 10)
+
+inptBox = wait.until(ec.element_to_be_clickable((By.XPATH, "//input[@name='query']")))
+inptBox.click()
+inptBox.send_keys(cityName)
+time.sleep(2)
+
+searchResults = driver.find_element(By.XPATH, "//div[@class='results-container']")
+result = searchResults.find_element(By.XPATH, f"//div[@class='search-bar-result search-result']")
+wait.until(ec.presence_of_element_located((By.XPATH, f"//div[@class='search-bar-result search-result']")))
+
+result.click()
+
+referenceTemp = wait.until(ec.presence_of_element_located((By.XPATH, "//div[@class='temp']")))
+details = wait.until(ec.presence_of_element_located((By.XPATH, "//div[@class='phrase']")))
+
+
+
 
 print(geoData)
-print(f"Weather in {cityName} is " + weatherData['weather'][0]['description'] + f" and the temperature is {weatherData['main']['temp']}º celsius whit a humidity of {weatherData['main']['humidity']}%")
+print(f"Weather in {cityName} is " + weatherData['weather'][0]['description'] + f" and the temperature is {weatherData['main']['temp']}ºC")
+print(f"Weather in {cityName} from the reference website accuweather is {details.text} the temperature  is {referenceTemp.text}")
+
+
